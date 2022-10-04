3bfcb9f534883aee2d352a20578a80311fafb6ea
AdrianS <subtirica123@gmail.com>
2022-10-05 00:47:25 +0300

    parsing json and data struct serialization

--- abi_serialization/src/main.rs
+++ abi_serialization/src/main.rs
@@ -1,26 +1,31 @@
+use serde::{Deserialize, Serialize};
 use elrond_codec::{
-    top_encode_from_nested, NestedEncodeNoErr,
+    top_encode_from_nested, top_encode_to_vec_u8,
     EncodeErrorHandler, NestedEncode, NestedEncodeOutput,
-    TopEncode, TopEncodeOutput, top_encode_to_vec_u8,
+    TopEncode, TopEncodeOutput,
 };
 
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub enum MyValueModel {
-    Int32,
-    Struct { a: u32, b: u32 },
+
+#[derive(Serialize, Deserialize)]
+struct MyValueModel {
+    name: String,
+    int: u16,
+    seq: Vec<u8>,
+    uint_64: u64,
 }
 
-impl NestedEncodeNoErr for MyValueModel {
-    fn dep_encode_no_err<O: NestedEncodeOutput>(&self, dest: &mut O) {
-        match self {
-            MyValueModel::Int32 => {
-                0i32.dep_encode_no_err(dest);
-            },
-            MyValueModel::Struct { a, b } => {
-                a.dep_encode_no_err(dest);
-                b.dep_encode_no_err(dest);
-            },
-        }
+impl NestedEncode for MyValueModel {
+    fn dep_encode_or_handle_err<O, H>(&self, dest: &mut O, h: H) -> Result<(), H::HandledErr>
+    where
+        O: NestedEncodeOutput,
+        H: EncodeErrorHandler,
+    {
+        self.name.dep_encode_or_handle_err(dest, h)?;
+        self.int.dep_encode_or_handle_err(dest, h)?;
+        self.seq.dep_encode_or_handle_err(dest, h)?;
+        self.uint_64.dep_encode_or_handle_err(dest, h)?;
+
+        Ok(())
     }
 }
 
@@ -35,22 +40,17 @@ impl TopEncode for MyValueModel {
     }
 }
 
-impl NestedEncode for MyValueModel {
-    #[inline]
-    fn dep_encode_or_handle_err<O, H>(&self, dest: &mut O, _h: H) -> Result<(), H::HandledErr>
-    where
-        O: NestedEncodeOutput,
-        H: EncodeErrorHandler,
-    {
-        self.dep_encode_no_err(dest);
-        Ok(())
-    }
-}
-
 fn main() {
-    //let s = MyValueModel::Struct { a: 1, b: 5 };
-    //let result_bytes = top_encode_to_vec_u8(&s).unwrap(); 
-    //println!("Res: {:?}", result_bytes);
+    let json_data = r#"
+    {
+        "name": "aaa",
+        "int": 20,
+        "seq": [1, 2, 3, 4],
+        "uint_64": 200
+    }"#;
 
-    
+    let p: MyValueModel = serde_json::from_str(json_data).unwrap();
+
+    let result_bytes = top_encode_to_vec_u8(&p).unwrap(); 
+    println!("Res: {:?}", result_bytes);
 }
