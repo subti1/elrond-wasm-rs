a816d687fe4a5a18dbf574c888cc4623200112de
AdrianS <subtirica123@gmail.com>
2022-10-05 01:16:03 +0300

    Implement TopDecode for MyValueModel

--- abi_serialization/src/main.rs
+++ abi_serialization/src/main.rs
@@ -1,9 +1,11 @@
 use serde::{Deserialize, Serialize};
 use elrond_codec::{
-    top_encode_from_nested, top_encode_to_vec_u8,
-    EncodeErrorHandler, NestedEncode, NestedEncodeOutput,
-    TopEncode, TopEncodeOutput,
+    top_encode_to_vec_u8, DefaultErrorHandler,
+    top_decode_from_nested_or_handle_err, top_encode_from_nested, DecodeErrorHandler,
+    EncodeErrorHandler, NestedDecode, NestedDecodeInput, NestedEncode, NestedEncodeOutput,
+    TopDecode, TopDecodeInput, TopEncode, TopEncodeOutput,
 };
+use std::fmt; // Import `fmt`
 
 
 #[derive(Serialize, Deserialize)]
@@ -12,6 +14,14 @@ struct MyValueModel {
     int: u16,
     seq: Vec<u8>,
     uint_64: u64,
+}
+
+// the trait `fmt::Display` is implemented
+impl fmt::Display for MyValueModel {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "name:{}, int:{}, seq:[{:?}], uint_64:{}", self.name, self.int, self.seq, self.uint_64)
+    }
 }
 
 impl NestedEncode for MyValueModel {
@@ -40,6 +50,31 @@ impl TopEncode for MyValueModel {
     }
 }
 
+impl NestedDecode for MyValueModel {
+    fn dep_decode_or_handle_err<I, H>(input: &mut I, h: H) -> Result<Self, H::HandledErr>
+    where
+        I: NestedDecodeInput,
+        H: DecodeErrorHandler,
+    {
+        Ok(MyValueModel {
+            name: String::dep_decode_or_handle_err(input, h)?,
+            int: u16::dep_decode_or_handle_err(input, h)?,
+            seq: Vec::<u8>::dep_decode_or_handle_err(input, h)?,
+            uint_64: u64::dep_decode_or_handle_err(input, h)?,
+        })
+    }
+}
+
+impl TopDecode for MyValueModel {
+    fn top_decode_or_handle_err<I, H>(input: I, h: H) -> Result<Self, H::HandledErr>
+    where
+        I: TopDecodeInput,
+        H: DecodeErrorHandler,
+    {
+        top_decode_from_nested_or_handle_err(input, h)
+    }
+}
+
 fn main() {
     let json_data = r#"
     {
@@ -52,5 +87,10 @@ fn main() {
     let p: MyValueModel = serde_json::from_str(json_data).unwrap();
 
     let result_bytes = top_encode_to_vec_u8(&p).unwrap(); 
+
     println!("Res: {:?}", result_bytes);
+
+    let result_obj = MyValueModel::top_decode_or_handle_err(result_bytes, DefaultErrorHandler).unwrap();
+
+    println!("{}", result_obj);
 }
